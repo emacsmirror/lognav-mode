7bd99675ea82e7c9f2d04b1dd5804f8eb8b0309b
Shawn Ellis <shawn.ellis17@gmail.com>
2017-12-28 00:09:04 -0600

    Reformatted the documentation

--- lognav-mode.el
+++ lognav-mode.el
@@ -28,10 +28,11 @@
 
 ;; Lognav-mode is a minor mode used for finding and navigating errors within a
 ;; buffer or a log file.  The keybinding M-n moves the cursor to the first error
-;; within the log file.  M-p moves the cursor to the previous error.  Lognav-mode
-;; only highlights the errors that are visible on the screen rather than
-;; highlighting all errors found within the buffer.  This is especially useful
-;; when opening up large log files for analysis.
+;; within the log file.  M-p moves the cursor to the previous error.
+;;
+;; Lognav-mode only highlights the errors that are visible on the screen rather
+;; than highlighting all errors found within the buffer.  This is especially
+;; useful when opening up large log files for analysis.
 
 ;; Add the following line in your .emacs file to use Lognav-mode:
 ;;
